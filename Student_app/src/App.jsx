89246fd6ff3701f1e10ce4b300d7d334a312e486
Kavvya <prajapatikavvya@gmail.com>
2026-04-14 18:03:43 +0530

    Initial commit

---
+++ Student_app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Student_Form from './Compo1/Student_Form'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Student_Form />
+    </>
+  )
+}
+
+export default App
---
+++ Student_app/src/Compo1/Student_Form.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import './Student_Form.css'
+
+
+export default function Student_Form() {
+    const [learnerName, setLearnerName] = useState('')  
+    const [learnerAge, setLearnerAge] = useState('')
+    const [courseTitle, setCourseTitle] = useState('')
+    
+    const handleSubmission = (e) => {
+        e.preventDefault()
+        if (!learnerName || !learnerAge || !courseTitle) {
+            alert('Please fill in all the fields to proceed with enrollment.');
+            return
+        }
+
+        const newStudent ={
+            fullName: learnerName,
+            age: learnerAge,
+            course: courseTitle,
+        }
+
+        onAddLearner(newStudent)
+        setLearnerName('')
+        setLearnerAge('')
+        setCourseTitle('')
+    };
+
+
+  return (
+    
+    <div className="form-container">
+        <div className='card'>
+      
+      <h2>Student Form</h2>
+      
+      <form onSubmit={handleSubmission}>
+        <div>
+          {/* Modified Labels [cite: 76] */}
+          <label>Full Name: </label>
+          <input 
+            type="text" 
+            value={learnerName} 
+            onChange={(e) => setLearnerName(e.target.value)} 
+          />
+        </div>
+        
+        <div>
+          <label>Current Age: </label>
+          <input 
+            type="number" 
+            value={learnerAge} 
+            onChange={(e) => setLearnerAge(e.target.value)} 
+          />
+        </div>
+
+        <div>
+          <label>Course Selection: </label>
+          <input 
+            type="text" 
+            value={courseTitle} 
+            onChange={(e) => setCourseTitle(e.target.value)} 
+          />
+        </div>
+
+        <div className="button-container">
+        <button type="submit" className="button">Submit</button>
+        </div>
+      </form>
+    </div>
+    </div>
+  )
+}
